979f9fb8063ac98995a1b7dcaed44107ca28f786
yez <s20184157@gmail.com>
2026-06-26 01:49:53 +0900

    f

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -2,6 +2,13 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/index", (req, res) => {
+  res.json({
+    ok: true,
+    message: "api works",
+  });
+});
+
 router.get("/healthz", (req, res) => {
   res.status(200).send("HEALTH OK");
 });
