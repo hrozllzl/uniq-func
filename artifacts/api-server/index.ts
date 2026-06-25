70e6a1bb505f00281fd380477a623c9e8899084a
Yejee Sa <98683722+hrozllzl@users.noreply.github.com>
2026-06-25 23:09:57 +0900

    Update index.ts

--- artifacts/api-server/index.ts
+++ artifacts/api-server/index.ts
@@ -1,3 +1,9 @@
-import app from "../src/app";
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", routes);
 
 export default app;
