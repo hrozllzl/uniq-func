b82a9dd6431eabf1f01f572ecd1f21bf0f3f8bf4
Yejee Sa <98683722+hrozllzl@users.noreply.github.com>
2026-06-26 01:01:36 +0900

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,34 +1,12 @@
-import express, {
-  type Express,
-  type Request,
-  type Response,
-  type NextFunction,
-} from "express";
-import cors from "cors";
-import router from "./routes/index.js";
-import { logger } from "./lib/logger.js";
+import express from "express";
 
-const app: Express = express();
+const app = express();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const start = Date.now();
-
-  res.on("finish", () => {
-    logger.info({
-      method: req.method,
-      url: req.url,
-      statusCode: res.statusCode,
-      duration: Date.now() - start,
-    });
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+    message: "express works"
   });
-
-  next();
 });
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", router);
 
 export default app;
